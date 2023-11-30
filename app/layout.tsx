c3e3d5e7c08641184ee2ee1cd1b9602a5abab376
Joncoded <joncoded@gmail.com>
2023-11-29 23:54:27 -0500

    adding head component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Head from "../components/head"
 import "./global.css"
 
 export const metadata = {
@@ -12,7 +13,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Head />
+        {children}
+      </body>
     </html>
   )
 }
---
+++ components/head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <div className="bg-sky-700 text-white w-full sticky z-40 p-5">
+      <div className="max-w-screen-lg mx-auto">
+        <h1 className="font-mono text-2xl">jonovert</h1>
+      </div>
+    </div>
+  )
+}
