1682ced600ae772d832111a3317850cfcd599a6a
Joncoded <joncoded@gmail.com>
2023-11-30 15:20:04 -0500

    populating about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,9 +4,9 @@ import { MainDiv } from "../../components/main"
 export default function About() {
   return ( 
     <MainDiv>
-      <h2 className="text-3xl">About</h2>
-      <p>This is my about page!</p>
-      <p><Link href="/">Go back to main page</Link></p>
+      <h2 className="text-5xl font-light">About</h2>
+      <p>I built this simple app to help my write about Next.js + JSON + various units of measurements!</p>
+      <p><span aria-hidden="true">&larr;</span> <Link href="/">Go back to main page</Link></p>
     </MainDiv>  
   )
 
