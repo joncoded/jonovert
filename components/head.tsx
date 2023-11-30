04b0fc301648a226e4a13c397e25ac791616b971
Joncoded <joncoded@gmail.com>
2023-11-30 15:19:42 -0500

    updating header properties

--- components/head.tsx
+++ components/head.tsx
@@ -1,8 +1,10 @@
+import Link from "next/link"
+
 export default function Head() {
   return (
-    <div className="bg-sky-700 text-white w-full sticky z-40 p-5">
+    <div className="bg-sky-700 text-white w-full sticky top-0 z-40 p-5">
       <div className="max-w-screen-lg mx-auto">
-        <h1 className="font-mono text-2xl">jonovert</h1>
+        <h1 className="text-4xl"><Link href="/">jonovert</Link></h1>
       </div>
     </div>
   )
