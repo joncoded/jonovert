bab9ba08327ef11ab050a7f714c85026f69d57ac
Joncoded <joncoded@gmail.com>
2023-12-02 00:06:59 -0500

    updating home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,41 @@
-import Link from "next/link"
+'use client'
+
 import { MainDiv } from "../components/main"
+import { useRouter } from "next/navigation"
+import Link from "next/link"
 
 export default function Home() {
+
+  const router = useRouter()
+
+  const handleMainMenuChange = (e: any) => {
+    e.preventDefault()
+    router.push(e.target.value)
+  }
+  
   return (
     <MainDiv>
       
       <h2 className="text-5xl font-light">Hello!</h2>
 
-      <p>What would you like to convert now?</p>
+      <p>Welcome to my measurement converter!</p> 
 
-      <ul>
-        <li><Link href="/length">Length</Link></li>
-      </ul>
+      <select 
+        className="head-menu bg-black text-white my-5 py-2 px-5 text-xl w-full" 
+        onChange={handleMainMenuChange}        
+      >
+        <option>What would you like to convert today?</option>        
+        <option value="/length">length</option>
+        <option value="/mass">mass</option>
+        <option value="/time">time</option>          
+      </select>
 
-      <p>Read more?</p>
-
-      <ul>
-        <li><Link href="/about">About</Link></li>
-      </ul>
+      <div className="flex flex-col sm:flex-row gap-5 mt-10">
+        <Link href="/about">about</Link>
+        <Link href="https://github.com/joncoded/jonovert">github</Link>
+        <Link href="https://github.com/joncoded/jonovert">joncoded</Link>
+      </div>
+      
       
     </MainDiv>
   )
