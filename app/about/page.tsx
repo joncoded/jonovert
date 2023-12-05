c88742bae2dffcce60c0d56cb858392fef864461
Joncoded <joncoded@gmail.com>
2023-12-05 09:53:55 -0500

    updating about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -7,7 +7,7 @@ export default function About() {
       
       <h2 className="text-5xl font-light">About</h2>
       
-      <p>I built this simple web app to help me review and learn about:</p> 
+      <p>I (@<Link href="https://github.com/joncoded">joncoded</Link>) built this simple web app to help me review and learn about:</p> 
       
       <ul>
         <li><Link href="https://nextjs.org" target="_blank">Next.js</Link> (a way to build websites)</li>
@@ -17,21 +17,24 @@ export default function About() {
         <li><Link href="https://en.wikipedia.org/wiki/Unit_of_measurement" target="_blank">various units of measurements</Link></li>
       </ul>
       
-      <p>As a <strong>Canadian</strong>, I also often need to convert <strong>American</strong> units of measure into metric, and vice versa!</p> 
+      <p>Also, as a <strong>Canadian</strong>, I often need to convert <strong>American</strong> units of measure into metric (and vice versa)</p> 
       
-      <p>So, I have created this website to work more for me, rather than the other way around!</p>
+      <p>So, I have created this microsite to work more for me, rather than the other way around!</p>
 
       <h3 className="text-3xl font-light">Progress</h3>
 
-      <p>The "minimal viable product" is complete!</p>
-      
-      <p>I will <Link href="https://github.com/joncoded/jonovert/commits/master">report more conversion data</Link> when time permits!</p>
+      <ul>
+
+        <li>The "minimal viable product" are complete - this is a working prototype</li>
+        <li><Link href="https://github.com/joncoded/jonovert/commits/master">More conversion data</Link> will come when time permits</li>
+
+      </ul>          
 
       <hr className="my-5" />
       
       <p>
         <span aria-hidden="true" className="mr-2">&larr;</span> 
-        <Link href="/">Let's get back to converting!</Link>
+        <Link href="/">Let's get back to converting</Link>
       </p>
     </MainDiv>  
   )
